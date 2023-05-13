5a44ad23f37fad96a2a101c87543f0af967d9793
Milan Wróblewski <01161701@pw.edu.pl>
2023-05-13 11:50:41 +0200

    proggress

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let x_res = 50;
-let y_res = 50;
+let x_res = 10;
+let y_res = 10;
 let m_width = 400;
 let m_height = 400;
 let px_size_x = m_width / x_res;
@@ -9,44 +9,65 @@ let cells = initialize_arr(x_res, y_res, 0);
 
 function setup() {
     createCanvas(m_width, m_height);
-    console.log(cells[10][0].type);
     allocate_cells();
-    console.log(cells[10][0].type);
 }
 
 function draw() {
-    background(255);
-    square(40, 40, 80);
-    line(0, 0, 80, 80);
-    ellipse(200, 200, 161, 80);
+    noLoop(background(255));
+    noLoop(square(60, 100, 20));
+    noLoop(line(0, 0, 80, 80));
+    noLoop(ellipse(200, 200, 161, 80));
     noLoop(find_walls());
     draw_grid();
+    console.log(cells[10][0].updateFlow());
+    console.log(cells[10][0].type);
+    console.log(cells[10][1].updateFlow());
 }
 
-
 class Cell {
-    constructor(topCell = null, leftCell = null, bottomCell = null, rightCell = null, type = 'ff') {
-        this.adjacentsTLBR = [topCell, leftCell, bottomCell, rightCell];
-        this.flow_left = leftCell ? leftCell.flow_right : 0;
-        this.flow_top = topCell ? topCell.flow_top : 0;
+    constructor({
+        x,
+        y,
+        type = 'ff'
+    } = {}) {
+        this.x = x;
+        this.y = y;
+        this.flow_left = 0;
+        this.flow_top = 0;
         this.flow_right = 0;
         this.flow_bottom = 0;
         this.setType(type);
+
     }
 
     setType(type) {
         this.type = type;
+        if (type == 'src') {
+            this.setFlow({
+                right: 1
+            });
+            // this.flow_right = 1;
+        }
     }
 
-    setAdjacent(top = null, left = null, right = null, bottom = null) {
-        let _top = top ? top : this.adjacentsTLBR[0];
-        let _left = left ? left : this.adjacentsTLBR[1];
-        let _bottom = bottom ? bottom : this.adjacentsTLBR[2];
-        let _right = right ? right : this.adjacentsTLBR[3];
-        this.adjacentsTLBR = [_top, _left, _bottom, _right];
+    setAdjacent({
+        top = null,
+        left = null,
+        right = null,
+        bottom = null
+    } = {}) {
+        this.a_top = top ? top : this.a_top;
+        this.a_left = left ? left : this.a_left;
+        this.a_bottom = bottom ? bottom : this.a_bottom;
+        this.a_right = right ? right : this.a_right;
     }
 
-    setFlow(top = null, left = null, right = null, bottom = null) {
+    setFlow({
+        top = null,
+        left = null,
+        right = null,
+        bottom = null
+    } = {}) {
         this.flow_top = top ? top : this.flow_top;
         this.flow_left = left ? left : this.flow_left;
         this.flow_right = right ? right : this.flow_right;
@@ -65,13 +86,6 @@ class Cell {
             this.flow_left = 0;
         }
     }
-    getDiv() {
-        sum = this.flow_left;
-        sum += this.flow_top;
-        sum -= this.flow_right;
-        sum -= this.flow_bottom;
-        return sum;
-    }
     getColor() {
         switch (this.type) {
             case 'wa':
@@ -82,9 +96,23 @@ class Cell {
                 return (this.getEdgesCount() * 255 / 4);
         }
     }
+
+    updateFlow() {
+        this._getFlowTopLeft();
+        if (this.type != 'src')this.decompress();
+        return this.getDiv();
+    }
+
+    getDiv() {
+        let sum = this.flow_left ? this.flow_left : 0;
+        sum += this.flow_top ? this.flow_top : 0;
+        sum -= this.flow_right ? this.flow_right : 0;
+        sum -= this.flow_bottom ? this.flow_bottom : 0;
+        return sum;
+    }
     decompress() {
-        div = this.getDiv();
-        edges = this.getEdgesCount();
+        let div = this.getDiv();
+        let edges = this.getEdgesCount();
         this.flow_left -= div / edges;
         this.flow_top -= div / edges;
         this.flow_right += div / edges;
@@ -93,55 +121,27 @@ class Cell {
     getEdgesCount() {
         if (this.edges) return this.edges;
         let edges = 0;
-        for (const c of this.adjacentsTLBR) {
-            edges += (c && c.type == 'ff') ? 1 : 0;
-        }
+        let x = this.x;
+        let y = this.y;
+        // console.log(cells[y][x + 1].type); // TODO getter
+        edges += (x > 0 && cells[y][x - 1].type == 'ff') ? 1 : 0;
+        edges += (x<x_res && cells[y][x + 1].type == 'ff') ? 1 : 0;
+        edges += (y>0 && cells[y-1][x].type == 'ff') ? 1 : 0;
+        edges += (y<y_res && cells[y+1][x].type == 'ff') ? 1 : 0;
+        this.edges = edges;
+        // console.log(edges);
         return edges;
     }
 }
 //each cell stores ff, wa, src value deterimining its type
 
 function allocate_cells() {
-    let x = 0;
-    let y = 0;
-    for (y = 0; y < y_res; ++y) {
-    console.log(cells[y][x].type);
-    cells[y][x] = new Cell(type = 'src');
-    console.log(cells[y][x].type);
-}
-    for (y = 0; y < y_res; ++y) {
-        for (x = 1; x < x_res; ++x) {
-            cells[y][x] = new Cell();
-        }
-    }
-
-    for (y = 0; y < y_res; ++y) {
-        for (x = 0; x < x_res; ++x) {
-            let t = null;
-            let l = null;
-            let b = null;
-            let r = null;
-            try {
-                t = cells[y - 1][x];
-            } catch (ValueError) {}
-            try {
-                l = cells[y][x - 1];
-            } catch (ValueError) {}
-            try {
-                b = cells[y + 1][x];
-            } catch (ValueError) {}
-            try {
-                r = cells[y][x + 1];
-            } catch (ValueError) {}
-            cells[y][x].setAdjacent(t, l, r, b);
+    for (let y = 0; y < y_res; ++y) {
+        for (let x = 0; x < x_res; ++x) {
+            cells[y][x] = new Cell(x, y);
         }
     }
 }
-
-function initialize_arr(x, y, val) {
-    return [...Array(y)].map(_ => Array(x).fill(val));
-}
-
 
 
 function draw_grid() {
@@ -162,15 +162,15 @@ function find_walls() {
         for (let x = 0; x < x_res; ++x) {
             b_x = x * px_size_x;
             b_y = y * px_size_y;
+            if (x == 0) {
+                cells[y][x].setType('src');
+                continue;
+            }
             if (scan_pixel(b_x, b_y)) {
-                set_type(x, y, 'wa');
+                cells[y][x].setType('wa');
             }
         }
     }
-}
-
-function set_type(x, y, type) {
-    cells[y][x].setType(type);
 }
 
 function scan_pixel(b_x, b_y) {
@@ -183,4 +183,8 @@ function scan_pixel(b_x, b_y) {
         }
     }
     return false;
+}
+
+function initialize_arr(x, y, val) {
+    return [...Array(y)].map(_ => Array(x).fill(val));
 }
